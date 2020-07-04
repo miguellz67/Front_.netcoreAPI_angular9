00b14805c6bc2ea60881a1784d273f0c43a84e28
miguellz67 <luizmiguel67h@outlook.com>
2020-07-04 18:38:14 -0300

    criacao e validacao formularios

--- src/app/estoque/estoque.component.ts
+++ src/app/estoque/estoque.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ProductService } from './../_services/product/product.service';
 import { CategoryService } from './../_services/category/category.service';
 import { Product } from './../_models/Product';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Category } from './../_models/Category';
 
 @Component({
   selector: 'app-estoque',
@@ -12,17 +14,67 @@ import { Product } from './../_models/Product';
 export class EstoqueComponent implements OnInit {
 
   products: Product[];
+  categories: Category[];
+  // tslint:disable-next-line: variable-name
+  _listFilter: string;
+  registerForm: FormGroup;
+  registerCat: FormGroup;
 
-  constructor(private http: HttpClient, private productService: ProductService, categoryService: CategoryService) { }
+  get listFilter(){
+   return this._listFilter;
+  }
+  set listFilter(value: string){
+    this._listFilter = value;
+  }
+
+  openModal(template: any){
+    template.show();
+  }
+
+  constructor(
+    private http: HttpClient,
+    private productService: ProductService,
+    private categoryService: CategoryService,
+    private fb: FormBuilder
+    ) { }
 
   ngOnInit() {
     this.getProducts();
+    this.getCategories();
+    this.validation();
+  }
+
+  validation(){
+    this.registerForm = this.fb.group({
+      name: ['', [Validators.minLength(15), Validators.maxLength(50), Validators.required]],
+      brand: ['', [Validators.minLength(2), Validators.required]],
+      model: ['', [Validators.minLength(5), Validators.required]],
+      desc: ['', [Validators.minLength(15), Validators.required]],
+      price: ['', [Validators.required, Validators.min(1)]],
+      category: ['', Validators.required],
+      amount: ['', [Validators.required, Validators.min(1)]]
+    });
+
+    this.registerCat = this.fb.group({})
+  }
+
+saveChanges(){
+
   }
 
   getProducts(){
     return this.productService.getProducts().subscribe((products: Product[]) => {
       this.products = products;
       console.log(products);
+    }, error => {
+      console.log(error.message);
+    });
+  }
+
+  getCategories(){
+    return this.categoryService.getCategories().subscribe((categories: Category[]) => {
+      this.categories = categories;
+      console.log(categories);
     }, error => {
       console.log(error.message);
     });
