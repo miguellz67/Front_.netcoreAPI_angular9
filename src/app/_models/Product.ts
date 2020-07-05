2dd141a0a0939b62c80dd92fa2dff5efc0bed23a
miguellz67 <luizmiguel67h@outlook.com>
2020-07-04 22:38:14 -0300

    categoryId adicionado a interface Product

--- src/app/_models/Product.ts
+++ src/app/_models/Product.ts
@@ -3,6 +3,7 @@ import { Guid } from 'guid-typescript';
 export interface Product {
     id: Guid;
     categoryName: string;
+    categoryId: Guid;
     name: string;
     brand: string;
     model: string;
